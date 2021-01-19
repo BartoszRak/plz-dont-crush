bbfc84623017054672f24ff836118641fec9bfca
Bartosz <bartosz.rak.official@gmail.com>
2021-01-19 02:46:58 +0100

    test(swapi 3rd party): turn off nestjs logger

--- test/third-party/swapi/main.ts
+++ test/third-party/swapi/main.ts
@@ -1,13 +1,15 @@
-import { AppModule } from './app.module'
 import { NestFactory } from '@nestjs/core'
-import { ThirdPartyPort } from '../third-party-port'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { AppModule } from './app.module'
+import { ThirdPartyPort } from '../third-party-port'
 import { setupApplication } from './setup-application'
 import { waitUntilFree } from 'tcp-port-used'
 
 export async function bootstrapSwapi() {
   await waitUntilFree(ThirdPartyPort.Swapi)
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: false,
+  })
   setupApplication(app)
   await app.listen(ThirdPartyPort.Swapi)
   return app
